de21b22e37654c62fc23e15d59af8432560e487a
callmeboge <bogebaga@gmail.com>
2020-07-10 13:37:16 +0800

    Delete all image assets, m/ and redaktur/ directory

--- redaktur/menu4.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "../config/koneksi.php";
-
-$cek=umenu_akses("?module=iklan",$_SESSION[sessid]);
-if($cek==1 OR $_SESSION[leveluser]=='admin'){
-echo "<li><a href='?module=iklan'><b>Iklan</b></a></li>";
-}
-
-?>
